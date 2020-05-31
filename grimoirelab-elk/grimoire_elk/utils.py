bf7dfbf10887599760375dc60033a7e14c591802
heming <lhming23@outlook.com>
2020-05-31 23:42:18 +0800

    Update utils.py

--- grimoirelab-elk/grimoire_elk/utils.py
+++ grimoirelab-elk/grimoire_elk/utils.py
@@ -2,8 +2,9 @@ from .raw.git import GitOcean
 from .enriched.git import GitEnrich
 import pkg_resources
 
+ENTRY_POINT_NAME = "grimoire_elk"
 def get_connectors():
     connectors = {"git": [GitOcean, GitEnrich]}
-    for entry_point in pkg_resources.iter_entry_points('grimoire_elk'):
+    for entry_point in pkg_resources.iter_entry_points(ENTRY_POINT_NAME):
         connectors.update(entry_point.load()())
     return connectors
